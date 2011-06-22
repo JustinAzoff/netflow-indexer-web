028c63d4d94e3f9e429f1f83e21a7507720d53af
Justin Azoff <justin@bouncybouncy.net>
2011-06-22 14:07:51 -0400

    sorta working

---
+++ netflow_indexer_web.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+import sys
+from netflowindexer import config
+from netflowindexer.main import do_search
+import glob
+from bottle import Bottle, run, request
+
+CONFIG_FILE = "/data/nfdump_xap/nfdump.ini"
+
+#import bottle
+#bottle.debug(True)
+app = Bottle()
+
+@app.route("/search")
+def search():
+    ip   = request.GET.get('ip')
+    dump = request.GET.get('dump', False)
+    cfgdata = config.read_config(CONFIG_FILE)
+    for db in sorted(glob.glob(cfgdata['dbpath'] + "/*.db")):
+        for line in do_search(cfgdata['indexer'], db, [ip], dump):
+            yield line + "\n"
+
+@app.get("")
+@app.get("/")
+@app.get("/form")
+def form():
+    return """
+<html>
+<body>
+
+<form action="search">
+<fieldset>
+<legend>Search Netflow</legend>
+<label for="ip">Address</dump>
+<input type="text" name="ip" id="ip" /> <br />
+
+<label for="dump">Dump?</dump>
+<input type="checkbox" name="dump" id="dump" /> <br />
+
+<input type="submit" value="Search"/>
+</form>
+</body>
+</html>
+"""
+
+def main():
+    run(app, server='cgi')
+
+if __name__ == "__main__":
+    main()
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import sys, os
+from glob import glob
+
+
+version = '0.1'
+
+setup(name='netflowindexer_web',
+    version=version,
+    description="Simple web interface for netflow-indexer",
+    long_description="""\
+    """,
+    classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
+    keywords='netflowindexer web',
+    author='Justin Azoff',
+    author_email='JAzoff@uamail.albany.edu',
+    url='',
+    license='MIT',
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    py_modules=['netflow_indexer_web'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        "bottle",
+        "netflowindexer>=0.1.18",
+    ],
+    scripts=glob('scripts/*'),
+    entry_points = {
+        'console_scripts': [
+            'netflow-indexer-web = netflow_indexer_web:main',
+        ]
+    },
+    )
