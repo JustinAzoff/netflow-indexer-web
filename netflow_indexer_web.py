575c2359506f000bfac4f49be62957ac08b7fa9e
Justin Azoff <justin@bouncybouncy.net>
2011-06-29 16:34:15 -0400

    use new search api, allow choosing databases

--- netflow_indexer_web.py
+++ netflow_indexer_web.py
@@ -1,61 +1,69 @@
 #!/usr/bin/env python
 import sys
-from netflowindexer import config
-from netflowindexer.main import get_searcher
-import glob
+from netflowindexer import Searcher
 from bottle import Bottle, run, request, response
+from bottle import template
 import re
 
-CONFIG_FILE = "/data/nfdump_xap/nfdump.ini"
+CONFIG_FILE = "/spare/tmp/netflow/nfdump.ini"
 
-#import bottle
-#bottle.debug(True)
+import bottle
+bottle.debug(True)
 app = Bottle()
-
-def do_search(indexer_type, database, ips, dump=None,filter=None):
-    searcher = get_searcher(indexer_type)
-    s = searcher(database)
-    return s.search(ips, dump, filter)
-
 
 @app.route("/search")
 def search():
     response.content_type = "text/plain"
     ip   = request.GET.get('ip','').strip()
+    databases = request.GET.getall('databases')
     if not ip:
         yield 'missing ip parameter'
         return
     ips = re.split("[, ]+", ip)
 
     dump = request.GET.get('dump', False)
-    cfgdata = config.read_config(CONFIG_FILE)
 
-    for db in sorted(glob.glob(cfgdata['dbpath'] + "/*.db")):
-        for line in do_search(cfgdata['indexer'], db, ips, dump):
-            yield str(line) + "\n"
+    s = Searcher(CONFIG_FILE)
+    if not databases:
+        databases = s.list_databases()
 
-@app.get("")
-@app.get("/")
-@app.get("/form")
-def form():
-    return """
+    for d in databases:
+        for line in s.search(d, ips, dump):
+                yield str(line) + "\n"
+
+TEMPLATE = """
 <html>
 <body>
 <h1>Netflow-Indexer Search</h1>
 <form action="search">
 <fieldset>
 <legend>Search Netflow</legend>
-<label for="ip">Address</dump>
+<label for="ip">One or more addresses</label>
 <input type="text" name="ip" id="ip" size="80" /> <br />
 
-<label for="dump">Dump?</dump>
+<label for="dump">Dump?</label>
 <input type="checkbox" name="dump" id="dump" /> <br />
 
+<label for="databases">Databases?</label> <br/>
+<select name="databases" multiple="multiple">
+%for d in databases:
+    <option value="{{d}}" selected="selected">{{d}}</option>
+%end for
+</select> <br />
 <input type="submit" value="Search"/>
 </form>
 </body>
 </html>
 """
+
+
+@app.get("")
+@app.get("/")
+@app.get("/form")
+def form():
+    s = Searcher(CONFIG_FILE)
+    databases = s.list_databases()
+    return template(TEMPLATE, databases=databases)
 
 def main():
     run(app, server='cgi')
