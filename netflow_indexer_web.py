cb592188b6ca05bb0cbfb29b97dd7c26be41a296
Justin Azoff <justin@bouncybouncy.net>
2011-06-30 11:41:22 -0400

    various improvements
    * Add filter option
    * De-select database by default
    * un-hardcode config file

--- netflow_indexer_web.py
+++ netflow_indexer_web.py
@@ -5,16 +5,16 @@ from bottle import Bottle, run, request, response
 from bottle import template
 import re
 
-CONFIG_FILE = "/spare/tmp/netflow/nfdump.ini"
 
 import bottle
-bottle.debug(True)
+#bottle.debug(True)
 app = Bottle()
 
 @app.route("/search")
 def search():
     response.content_type = "text/plain"
     ip   = request.GET.get('ip','').strip()
+    filter    = request.GET.get('filter','').strip()
     databases = request.GET.getall('databases')
     if not ip:
         yield 'missing ip parameter'
@@ -23,35 +23,48 @@ def search():
 
     dump = request.GET.get('dump', False)
 
-    s = Searcher(CONFIG_FILE)
+    s = Searcher(app.config['nfi_config'])
     if not databases:
         databases = s.list_databases()
 
     for d in databases:
-        for line in s.search(d, ips, dump):
+        for line in s.search(d, ips, dump, filter):
                 yield str(line) + "\n"
 
 TEMPLATE = """
 <html>
+<head>
+<style type="text/css">
+label {
+    font-weight: bold;
+}
+</style>
+</head>
 <body>
 <h1>Netflow-Indexer Search</h1>
 <form action="search">
 <fieldset>
 <legend>Search Netflow</legend>
-<label for="ip">One or more addresses</label>
-<input type="text" name="ip" id="ip" size="80" /> <br />
+<label for="ip">One or more addresses</label> <br>
+<input type="text" name="ip" id="ip" size="80" /> <br>
 
-<label for="dump">Dump?</label>
-<input type="checkbox" name="dump" id="dump" /> <br />
+<label for="filter">Optional filter</label> <br>
+<input type="text" name="filter" id="filter" size="80" /> <br>
 
-<label for="databases">Databases?</label> <br/>
-<select name="databases" multiple="multiple">
+<label for="dump">Dump records?</label> <br>
+<input type="checkbox" name="dump" id="dump" /> <br>
+
+<input type="submit" value="Search"/> <br/>
+<label for="databases">Limit Databases?</label> <br>
+<select id="databases" name="databases" multiple="multiple" size=50>
 %for d in databases:
-    <option value="{{d}}" selected="selected">{{d}}</option>
+    <option value="{{d}}">{{d}}</option>
 %end for
-</select> <br />
-<input type="submit" value="Search"/>
+</select> <br>
 </form>
+<script>
+document.getElementById("databases").options[0].selected=false;
+</script>
 </body>
 </html>
 """
@@ -61,12 +74,14 @@ TEMPLATE = """
 @app.get("/")
 @app.get("/form")
 def form():
-    s = Searcher(CONFIG_FILE)
+    s = Searcher(app.config['nfi_config'])
     databases = s.list_databases()
     return template(TEMPLATE, databases=databases)
 
-def main():
-    run(app, server='cgi')
+def main(config):
+    app.config['nfi_config'] = config
+    run(app, server='auto', port=8000)
 
 if __name__ == "__main__":
-    main()
+    config = sys.argv[1]
+    main(config)
